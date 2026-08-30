6a700f18c5a9e89ce90184db90d9dc6079ee7c5e
well-okay-then <ericsoppi@proton.me>
2026-08-30 13:03:50 -0400

    done

--- main.rs
+++ main.rs
@@ -6,6 +6,6 @@ fn main() {
     let mut it = input.split_whitespace();
     let a: i64 = it.next().unwrap().parse().unwrap();
     let b: i64 = it.next().unwrap().parse().unwrap();
-    println!("{}", a + b); 
+    println!("{}", a * b); 
 
 }
