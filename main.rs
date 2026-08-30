3490f14eace7199e89cf2fa287f45e9f90c55144
well-okay-then <ericsoppi@proton.me>
2026-08-30 12:55:27 -0400

    done

--- main.rs
+++ main.rs
@@ -1,3 +1,11 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, Rust!"); 
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+    let mut it = input.split_whitespace();
+    let a: i64 = it.next().unwrap().parse().unwrap();
+    let b: i64 = it.next().unwrap().parse().unwrap();
+    println!("{}", a + b); 
+
 }
