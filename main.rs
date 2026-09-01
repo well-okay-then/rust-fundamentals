b6586ecf570e824b95e7f1ad158d7b72b5aaae8d
well-okay-then <ericsoppi@proton.me>
2026-08-31 23:49:59 -0400

    done

--- main.rs
+++ main.rs
@@ -1,18 +1,17 @@
 use std::io::{self, BufRead};
+use std::collections::HashSet;
 
 fn main() {
     let stdin = io::stdin();
     let line = stdin.lock().lines().next().unwrap().unwrap();
-    let nums: Vec<i64> = line.split_whitespace()
+    let input: Vec<String> = line.split_whitespace()
         .map(|s| s.parse().unwrap())
         .collect();
 
-    let mut best = std::i64::MIN;
-    for n in nums {
-        if n > best {
-            best = n;
-        }
+    let mut seen = HashSet::new();
+    for word in input {
+        seen.insert(word);
     }
 
-    println!("{}", best);
+    println!("{}", seen.len());
 }
