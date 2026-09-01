0493590b5fae833bf4e297abc1a78f78e42cf6d8
well-okay-then <ericsoppi@proton.me>
2026-08-31 22:57:08 -0400

    done

--- main.rs
+++ main.rs
@@ -3,12 +3,11 @@ use std::io::{self, BufRead};
 fn main() {
     let stdin = io::stdin();
     let mut iter = stdin.lock().lines();
-    let n: i32 = iter.next().unwrap().unwrap().trim().parse().unwrap();
+    let n: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
 
-    match (n % 3 == 0, n % 5 == 0) {
-        (true, true) => println!("FizzBuzz"),
-        (true, false) => println!("Fizz"),
-        (false, true) => println!("Buzz"),
-        (false, false) => println!("{}", n),
+    let mut total: i64 = 0;
+    for i in 1..=n {
+        total += i;
     }
+    println!("{}", total);
 }
