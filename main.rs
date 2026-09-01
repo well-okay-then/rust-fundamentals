71734df0f8b289707296db97f6688f45a75cccb2
well-okay-then <ericsoppi@proton.me>
2026-08-31 22:13:20 -0400

    done

--- main.rs
+++ main.rs
@@ -3,9 +3,6 @@ use std::io::Read;
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
-    let mut it = input.split_whitespace();
-    let a: i64 = it.next().unwrap().parse().unwrap();
-    let b: i64 = it.next().unwrap().parse().unwrap();
-    println!("{}", a * b); 
 
+    println!("{}", input.to_uppercase());
 }
