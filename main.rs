ebd76f9e30965d5810404ef1da5eb9b7b0e33802
well-okay-then <ericsoppi@proton.me>
2026-08-31 23:07:16 -0400

    done

--- main.rs
+++ main.rs
@@ -5,9 +5,9 @@ fn main() {
     let mut iter = stdin.lock().lines();
     let n: i64 = iter.next().unwrap().unwrap().trim().parse().unwrap();
 
-    let mut total: i64 = 0;
-    for i in 1..=n {
-        total += i;
-    }
-    println!("{}", total);
+    println!("{}", square(n));
+}
+
+fn square(n: i64) -> i64 {
+    n * n
 }
